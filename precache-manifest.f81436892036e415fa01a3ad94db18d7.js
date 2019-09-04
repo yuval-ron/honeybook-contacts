7282993794c45c0fb205f1f5fc0345b928f9347c
Yuval Ron <yuvaly2394@gmail.com>
2019-09-05 01:31:35 +0300

    Updates

---
+++ precache-manifest.f81436892036e415fa01a3ad94db18d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb436ea6c10964c98ca9f7b29325dd1e",
+    "url": "/honeybook-contacts/index.html"
+  },
+  {
+    "revision": "d376ae0e7fd61f4aeabf",
+    "url": "/honeybook-contacts/static/css/main.0814f4e5.chunk.css"
+  },
+  {
+    "revision": "9a3d92b563ef92c018be",
+    "url": "/honeybook-contacts/static/js/2.b8b490ab.chunk.js"
+  },
+  {
+    "revision": "d376ae0e7fd61f4aeabf",
+    "url": "/honeybook-contacts/static/js/main.22906993.chunk.js"
+  },
+  {
+    "revision": "622c73274e093ad11dcc",
+    "url": "/honeybook-contacts/static/js/runtime~main.4d5bd465.js"
+  }
+]);
