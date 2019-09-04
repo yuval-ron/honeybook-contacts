43e1907ebb70ef74042878f1c31353f33959efab
Yuval Ron <yuvaly2394@gmail.com>
2019-09-04 23:31:57 +0300

    Add design for contact item

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -2,11 +2,33 @@ import React, {Component} from 'react'
 
 export default class ContactItem extends Component {
   render() {
-    const {contact} = this.props
+    const {
+      contact: {
+        profile_image,
+        name,
+        company_name,
+        email,
+        icon,
+        job,
+        phone
+      }
+    } = this.props
+
+    const profilePictureStyle = {backgroundImage: `url(${profile_image})`}
+    const iconStyle = {backgroundImage: `url(${icon})`}
 
     return (
       <div className="contact-item-container">
-        {contact.name}
+        <div className="profile-picture" style={profilePictureStyle}></div>
+        <div className="details">
+          <div className="icon" style={iconStyle}></div>
+          <div className="fullname">{name}</div>
+          <div className="job-details">
+            <div className="job-title">{job}</div>
+            {job && company_name && <div className="separator"></div>}
+            <div className="company-name">@{company_name}</div>
+          </div>
+        </div>
       </div>
     )
   }
